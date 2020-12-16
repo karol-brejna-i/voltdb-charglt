ef6007388cee65cf2fae679f8927db06604eefd8
David Rolfe <drolfe@voltdb.com>
2020-12-16 10:52:22 +0000

    setup fix

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,8 +3,8 @@
 . $HOME/.profile
 
 cd
-cd voltdb-clite/ddl
+cd voltdb-charglt/ddl
 
 sqlcmd --servers=vdb1 < create_db.sql
 
-java -jar $HOME/bin/addtodeploymentdotxml.jar vdb1,vdb2,vdb3 deployment $HOME/voltdb-clite/scripts/export_and_import.xml
+java -jar $HOME/bin/addtodeploymentdotxml.jar vdb1,vdb2,vdb3 deployment $HOME/voltdb-charglt/scripts/export_and_import.xml
