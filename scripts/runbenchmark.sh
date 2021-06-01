41d3027a419e2bac01dad2d4356138c652728b15
David Rolfe <drolfe@voltdb.com>
2021-06-01 08:34:57 +0100

    Added date to runben chmark

--- scripts/runbenchmark.sh
+++ scripts/runbenchmark.sh
@@ -3,17 +3,21 @@
 cd
 cd voltdb-charglt/jars 
 
-kill -9 `ps -deaf | grep ChargingDemoTransactions.jar  | grep -v grep | awk '{ print $2 }'`
+# silently kill off any copy that is currently running...
+kill -9 `ps -deaf | grep ChargingDemoTransactions.jar  | grep -v grep | awk '{ print $2 }'` 2> /dev/null
 
 sleep 5 
 
 MX=$1
 CT=1
+DT=`date '+%Y%m%d_%H%M'`
 
 while
 	[ "${CT}" -le "${MX}" ]
 do
 
-	java -jar ChargingDemoTransactions.jar `cat $HOME/.vdbhostnames`  60000000 100 1200 60 > `uname -n`${CT}.lst &
+	java -jar ChargingDemoTransactions.jar `cat $HOME/.vdbhostnames`  60000000 100 1200 60 > ${DT}_`uname -n`_${CT}.lst &
 	CT=`expr $CT + 1`
 done
+
+wait 
