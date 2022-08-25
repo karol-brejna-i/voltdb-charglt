3bbccea8c770983ea3c6f29897938590b117566b
David Rolfe <drolfe@voltdb.com>
2022-08-25 10:54:22 +0100

    Create users on DB creation

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,3 +8,6 @@ cd voltdb-charglt/ddl
 sqlcmd --servers=vdb1 < create_db.sql
 
 java -jar $HOME/bin/addtodeploymentdotxml.jar vdb1,vdb2,vdb3 deployment $HOME/voltdb-charglt/scripts/export_and_import.xml
+
+cd ../jars
+java -jar CreateChargingDemoData.jar vdb1,vdb2,vdb3 5000000 10 100000
