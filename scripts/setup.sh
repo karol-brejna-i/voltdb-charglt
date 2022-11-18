a67743bf6a3fa3d7dfa1394627a694eac9b50ce5
David Rolfe <drolfe@voltdb.com>
2022-11-18 14:16:28 +0000

    faster create

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,4 +10,4 @@ sqlcmd --servers=vdb1 < create_db.sql
 java -jar $HOME/bin/addtodeploymentdotxml.jar vdb1,vdb2,vdb3 deployment $HOME/voltdb-charglt/scripts/export_and_import.xml
 
 cd ../jars
-java -jar CreateChargingDemoData.jar vdb1,vdb2,vdb3 5000000 10 100000
+java -jar CreateChargingDemoData.jar vdb1,vdb2,vdb3 5000000 30 100000
