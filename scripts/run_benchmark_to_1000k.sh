87b3270f1d316d67b3fcd65334779dedc934cd37
David Rolfe <drolfe@voltdb.com>
2023-06-28 15:59:27 +0100

    Renamed check_aws_io_limits.sh to check_aws_network_limits.sh

--- scripts/run_benchmark_to_1000k.sh
+++ scripts/run_benchmark_to_1000k.sh
@@ -9,11 +9,11 @@ then
 	exit 1
 fi
 
-sh check_aws_io_limits.sh
+sh check_aws_network_limits.sh
 sh -x runlargebenchmark.sh 90 2000 10 4000000 10  > ${BNAME}_oltp.lst
-sh check_aws_io_limits.sh
+sh check_aws_network_limits.sh
 sleep 60
 sh -x runlargekvbenchmark.sh 90 2000 10 4000000 100 50 10  > ${BNAME}_kv.lst
-sh check_aws_io_limits.sh
+sh check_aws_network_limits.sh
 
 
