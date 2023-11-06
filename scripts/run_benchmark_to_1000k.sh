16a720e982fc5e7e68b1edfac2bb5139cc413a01
David Rolfe <drolfe@voltdb.com>
2023-11-06 09:38:32 +0000

    Now stops when network throttled

--- scripts/run_benchmark_to_1000k.sh
+++ scripts/run_benchmark_to_1000k.sh
@@ -14,6 +14,6 @@ sh -x runlargebenchmark.sh 90 2000 10 4000000 10  > ${BNAME}_oltp.lst
 sh check_aws_network_limits.sh
 sleep 60
 sh -x runlargekvbenchmark.sh 90 2000 10 4000000 100 50 10  > ${BNAME}_kv.lst
-#sh check_aws_network_limits.sh
+sh check_aws_network_limits.sh
 
 
